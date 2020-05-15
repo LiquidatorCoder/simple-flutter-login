c63ba5fa70868a42887ce2e886035357be39b361
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-05-15 22:50:45 +0530

    Added basic login page ui

---
+++ lib/screens/input_form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class InputForm extends StatefulWidget {
+  @override
+  _InputFormState createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 720, height: 1440, allowFontScaling: true);
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/bg.png"),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Card(
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                ),
+              ),
+              margin: EdgeInsets.all(0),
+              color: Color(0xFFF2F2F2),
+              child: SizedBox(
+                height: 980.h,
+                width: 720.w,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
